f8c5a72fd22cfc07d5eb644fe3ad323e3dedd4b9
tferrerITBA <tferrer@itba.edu.ar>
2018-05-07 16:21:07 -0300

    Deleted Post from DB.

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504190810) do
+ActiveRecord::Schema.define(version: 20180507143442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
