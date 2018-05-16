db561955ba97fe66315ff46fdb6ce30fc8cfaecf
tferrerITBA <tferrer@itba.edu.ar>
2018-05-16 15:49:54 -0300

    Remove index and show from serializer.

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,13 +1,3 @@
 class BookSerializer < ActiveModel::Serializer
   attributes :id, :author, :title, :image, :publisher, :year, :genre, :created_at, :updated_at
-
-  def index
-    @books = Book.all
-    render json: @books, each_serializer: BookSerializer
-  end
-
-  def show
-    @book = Book.find(params[:id])
-    render json: @book, serializer: BookSerializer
-  end
 end
