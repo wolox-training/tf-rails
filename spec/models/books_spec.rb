7c7a61bfd73da1cd26943dd2d38253b5f1402683
tferrerITBA <tferrer@itba.edu.ar>
2018-05-18 10:39:16 -0300

    Remove irrelevant book model spec.

--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -7,8 +7,4 @@ describe Book, type: :model do
   it { validate_presence_of :title }
   it { validate_presence_of :publisher }
   it { validate_presence_of :year }
-
-  subject(:book) { build(:book) }
-
-  it { is_expected.to be_valid }
 end
